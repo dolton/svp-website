815e973f2df6afddcd29f9f94c8039bafbf876c6
nqe00225 <>
2021-04-28 11:53:32 +0530

    fixing css

--- src/app/website-mod/website.routing.ts
+++ src/app/website-mod/website.routing.ts
@@ -25,7 +25,7 @@ import { ReportsComponent } from './website/pages/catalogue/reports/reports.comp
 import { GetInvolvedComponent } from './website/pages/organization/get-involved/get-involved.component';
 
 export const routes: Routes = [
-    { path: '', component: WebsiteComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'org/about', component: AboutUsComponent },
     { path: 'org/founder', component: FounderComponent },
